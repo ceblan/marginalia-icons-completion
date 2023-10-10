50266e59f19a5b3f29add9d719c45e4b415da95b
Carlos <carlos@uberrimus.iterando.net>
2023-10-10 20:32:26 +0200

    rewording

--- marginalia-icons-completion.el
+++ marginalia-icons-completion.el
@@ -24,8 +24,8 @@
 
 ;;; Commentary:
 
-;; Add nerd-icons to completion candidates.
-;; nerd-icons-completion is inspired by
+;; Add marginalia-icons to completion candidates.
+;; marginalia-icons-completion is inspired by
 ;; `all-the-icons-completion': https://github.com/iyefrat/all-the-icons-completion
 
 ;;; Code:
@@ -34,34 +34,34 @@
 ;;(load-file "~/.local/share/icons-in-terminal/icons-in-terminal.el")
 (require 'icons-in-terminal)
 
-(defgroup nerd-icons-completion nil
+(defgroup marginalia-icons-completion nil
   "Add icons to completion candidates."
   :group 'appearance
   :group 'convenience
-  :prefix "nerd-icons-completion")
+  :prefix "marginalia-icons-completion")
 
-(defface nerd-icons-completion-dir-face
+(defface marginalia-icons-completion-dir-face
   '((t nil))
   "Face for the directory icon."
   :group 'icons-in-terminal-faces)
 
-(cl-defgeneric nerd-icons-completion-get-icon (_cand _cat)
+(cl-defgeneric marginalia-icons-completion-get-icon (_cand _cat)
   "Return the icon for the candidate CAND of completion category CAT."
   "")
 
-(cl-defmethod nerd-icons-completion-get-icon (cand (_cat (eql file)))
+(cl-defmethod marginalia-icons-completion-get-icon (cand (_cat (eql file)))
   "Return the icon for the candidate CAND of completion category file."
   (cond ((string-match-p "\\/$" cand)
          (concat
-          (icons-in-terminal-icon-for-dir cand :face 'nerd-icons-completion-dir-face)
+          (icons-in-terminal-icon-for-dir cand :face 'marginalia-icons-completion-dir-face)
           " "))
         (t (concat (icons-in-terminal-icon-for-file cand) " "))))
 
-(cl-defmethod nerd-icons-completion-get-icon (cand (_cat (eql project-file)))
+(cl-defmethod marginalia-icons-completion-get-icon (cand (_cat (eql project-file)))
   "Return the icon for the candidate CAND of completion category project-file."
-  (nerd-icons-completion-get-icon cand 'file))
+  (marginalia-icons-completion-get-icon cand 'file))
 
-(cl-defmethod nerd-icons-completion-get-icon (cand (_cat (eql buffer)))
+(cl-defmethod marginalia-icons-completion-get-icon (cand (_cat (eql buffer)))
   "Return the icon for the candidate CAND of completion category buffer."
   (let* ((mode (buffer-local-value 'major-mode (get-buffer cand)))
          (icon (all-the-icons-icon-for-mode mode))
@@ -76,13 +76,13 @@
      " ")))
 
 (autoload 'bookmark-get-filename "bookmark")
-(cl-defmethod nerd-icons-completion-get-icon (cand (_cat (eql bookmark)))
+(cl-defmethod marginalia-icons-completion-get-icon (cand (_cat (eql bookmark)))
   "Return the icon for the candidate CAND of completion category bookmark."
   (if-let (fname (bookmark-get-filename cand))
-      (nerd-icons-completion-get-icon fname 'file)
-    (concat (all-the-icons-octicon "bookmark" :face 'nerd-icons-completion-dir-face) " ")))
+      (marginalia-icons-completion-get-icon fname 'file)
+    (concat (all-the-icons-octicon "bookmark" :face 'marginalia-icons-completion-dir-face) " ")))
 
-(defun nerd-icons-completion-completion-metadata-get (orig metadata prop)
+(defun marginalia-icons-completion-completion-metadata-get (orig metadata prop)
   "Meant as :around advice for `completion-metadata-get', Add icons as prefix.
 ORIG should be `completion-metadata-get'
 METADATA is the metadata.
@@ -101,7 +101,7 @@ PROP is the property which is looked up."
                         (`(,cand ,prefix ,suffix)
                          (let ((orig (get-text-property 0 'multi-category cand)))
                            (list cand
-                                 (concat (nerd-icons-completion-get-icon (cdr orig) (car orig))
+                                 (concat (marginalia-icons-completion-get-icon (cdr orig) (car orig))
                                          prefix)
                                  suffix)))))
                     (funcall aff cands))))
@@ -111,7 +111,7 @@ PROP is the property which is looked up."
                       (pcase-exhaustive x
                         (`(,cand ,prefix ,suffix)
                          (list cand
-                               (concat (nerd-icons-completion-get-icon cand cat)
+                               (concat (marginalia-icons-completion-get-icon cand cat)
                                        prefix)
                                suffix))))
                     (funcall aff cands))))
@@ -119,12 +119,12 @@ PROP is the property which is looked up."
           (lambda (cands)
             (mapcar (lambda (x)
                       (let ((orig (get-text-property 0 'multi-category x)))
-                        (list x (nerd-icons-completion-get-icon (cdr orig) (car orig)) "")))
+                        (list x (marginalia-icons-completion-get-icon (cdr orig) (car orig)) "")))
                     cands)))
          (cat
           (lambda (cands)
             (mapcar (lambda (x)
-                      (list x (nerd-icons-completion-get-icon x cat) ""))
+                      (list x (marginalia-icons-completion-get-icon x cat) ""))
                     cands)))
          (aff)))
     (funcall orig metadata prop)))
@@ -132,17 +132,17 @@ PROP is the property which is looked up."
 ;; For the byte compiler
 (defvar marginalia-mode)
 ;;;###autoload
-(defun nerd-icons-completion-marginalia-setup ()
-  "Hook to `marginalia-mode-hook' to bind `nerd-icons-completion-mode' to it."
-  (nerd-icons-completion-mode (if marginalia-mode 1 -1)))
+(defun marginalia-icons-completion-marginalia-setup ()
+  "Hook to `marginalia-mode-hook' to bind `marginalia-icons-completion-mode' to it."
+  (marginalia-icons-completion-mode (if marginalia-mode 1 -1)))
 
 ;;;###autoload
-(define-minor-mode nerd-icons-completion-mode
+(define-minor-mode marginalia-icons-completion-mode
   "Add icons to completion candidates."
   :global t
-  (if nerd-icons-completion-mode
-      (advice-add #'completion-metadata-get :around #'nerd-icons-completion-completion-metadata-get)
-    (advice-remove #'completion-metadata-get #'nerd-icons-completion-completion-metadata-get)))
+  (if marginalia-icons-completion-mode
+      (advice-add #'completion-metadata-get :around #'marginalia-icons-completion-completion-metadata-get)
+    (advice-remove #'completion-metadata-get #'marginalia-icons-completion-completion-metadata-get)))
 
-(provide 'nerd-icons-completion)
-;;; nerd-icons-completion.el ends here
+(provide 'marginalia-icons-completion)
+;;; marginalia-icons-completion.el ends here
